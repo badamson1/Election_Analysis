3f49dabf0b355e3b255d00d7799ed15fa28ad90e
badamson1 <72579889+badamson1@users.noreply.github.com>
2020-12-06 22:53:21 -0800

    Add files via upload

---
+++ Python_practice.py
@@ -0,0 +1,9 @@
+counties = ["Arapahoe","Denver","Jefferson", "El Paso"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+for county in counties:
+    print(county)
+ counties_dict
+{'Arapahoe': 422829, 'Denver': 463353, 'Jefferson': 432438}
